0cba9a3a9961fe210a8dec7f28d3760ec617f1ad
aayubxon701 <aayubxon701@gmail.com>
2023-11-16 21:24:31 +0900

    feat:develope hompage related best restaurants sections

--- src/app/screens/HomePage/bestRestaurants.tsx
+++ src/app/screens/HomePage/bestRestaurants.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Favorite } from "@mui/icons-material";
-import { AspectRatio, Card, CardOverflow, CssVarsProvider, IconButton, Link, Typography } from "@mui/joy";
-import { Container, Box, Stack } from "@mui/material";
+import {
+  AspectRatio,
+  Card,
+  CardOverflow,
+  CssVarsProvider,
+  IconButton,
+  Link,
+  Typography,
+} from "@mui/joy";
+import { Container, Box, Stack, Button } from "@mui/material";
 import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
 import CallIcon from "@mui/icons-material/Call";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-
-
-
 
 const BestRestaurants = () => {
   return (
@@ -20,7 +25,7 @@ const BestRestaurants = () => {
       <Container sx={{ mt: "153px" }}>
         <Stack flexDirection={"column"} alignItems={"center"}>
           <Box className="category_title">Best Restaurants</Box>
-          <Stack sx={{mt:"45px"}} flexDirection={"row"}>
+          <Stack sx={{ mt: "45px" }} flexDirection={"row"}>
             <CssVarsProvider>
               <Card
                 sx={{
@@ -34,34 +39,46 @@ const BestRestaurants = () => {
                   <AspectRatio ratio={1}>
                     <img src="restaurant/girl.png" alt="" />
                     <IconButton
-                    aria-label="Like minimal photography"
-                    size="md"
-                    variant="solid"
-                    color="neutral"
-                    sx={{
-                      position: "absolute",
-                      zIndex: 2,
-                      borderRadius: "50%",
-                      right: "1rem",
-                      bottom: 0,
-                      transform: "translateY(50%)",
-                      color: "rgba(0,0,0,.4)",
-                    }}
-                  >
-                    <Favorite style={{ fill: "white" }} />
-                  </IconButton>
+                      aria-label="Like minimal photography"
+                      size="md"
+                      variant="solid"
+                      color="neutral"
+                      sx={{
+                        position: "absolute",
+                        zIndex: 2,
+                        borderRadius: "50%",
+                        right: "1rem",
+                        bottom: 0,
+                        transform: "translateY(50%)",
+                        color: "rgba(0,0,0,.4)",
+                      }}
+                    >
+                      <Favorite style={{ fill: "white" }} />
+                    </IconButton>
                   </AspectRatio>
                 </CardOverflow>
-                <Typography level="h2" sx={{fontSize:"md",mt:2}}>
-                   The Bruk Restaurants
-                  </Typography>
-                  <Typography level="title-sm" sx={{mt:0.1,mb: 2}}>
-                   <Link href="" textColor={"neutral.700"} startDecorator={<LocationOnRoundedIcon/>}>Turkey,Istanbul</Link>
-                  </Typography>
-                  <Typography level="title-sm" sx={{mt:0.1,mb: 2}}>
-                   <Link href="" textColor={"neutral.700"} startDecorator={<CallIcon/>}>+998950010001</Link>
-                  </Typography>
-                  <CardOverflow
+                <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
+                  The Bruk Restaurants
+                </Typography>
+                <Typography level="title-sm" sx={{ mt: 0.1, mb: 2 }}>
+                  <Link
+                    href=""
+                    textColor={"neutral.700"}
+                    startDecorator={<LocationOnRoundedIcon />}
+                  >
+                    Turkey,Istanbul
+                  </Link>
+                </Typography>
+                <Typography level="title-sm" sx={{ mt: 0.1, mb: 2 }}>
+                  <Link
+                    href=""
+                    textColor={"neutral.700"}
+                    startDecorator={<CallIcon />}
+                  >
+                    +998950010001
+                  </Link>
+                </Typography>
+                <CardOverflow
                   sx={{
                     display: "flex",
                     gap: 1.5,
@@ -98,7 +115,7 @@ const BestRestaurants = () => {
                     >
                       100{" "}
                       <VisibilityIcon
-                        sx={{ fontSize: 20, marginLeft: "5px"}}
+                        sx={{ fontSize: 20, marginLeft: "5px" }}
                       />
                     </Typography>
                     <Box
@@ -119,9 +136,7 @@ const BestRestaurants = () => {
                       }}
                     >
                       <div>500</div>
-                      <Favorite
-                        sx={{ fontSize: 20, marginLeft: "5px" }}
-                      />
+                      <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
                     </Typography>
                   </Box>
                 </CardOverflow>
@@ -138,34 +153,46 @@ const BestRestaurants = () => {
                   <AspectRatio ratio={1}>
                     <img src="restaurant/girl.png" alt="" />
                     <IconButton
-                    aria-label="Like minimal photography"
-                    size="md"
-                    variant="solid"
-                    color="neutral"
-                    sx={{
-                      position: "absolute",
-                      zIndex: 2,
-                      borderRadius: "50%",
-                      right: "1rem",
-                      bottom: 0,
-                      transform: "translateY(50%)",
-                      color: "rgba(0,0,0,.4)",
-                    }}
-                  >
-                    <Favorite style={{ fill: "white" }} />
-                  </IconButton>
+                      aria-label="Like minimal photography"
+                      size="md"
+                      variant="solid"
+                      color="neutral"
+                      sx={{
+                        position: "absolute",
+                        zIndex: 2,
+                        borderRadius: "50%",
+                        right: "1rem",
+                        bottom: 0,
+                        transform: "translateY(50%)",
+                        color: "rgba(0,0,0,.4)",
+                      }}
+                    >
+                      <Favorite style={{ fill: "white" }} />
+                    </IconButton>
                   </AspectRatio>
                 </CardOverflow>
-                <Typography level="h2" sx={{fontSize:"md",mt:2}}>
-                   The Bruk Restaurants
-                  </Typography>
-                  <Typography level="title-sm" sx={{mt:0.1,mb: 2}}>
-                   <Link href="" textColor={"neutral.700"} startDecorator={<LocationOnRoundedIcon/>}>Turkey,Istanbul</Link>
-                  </Typography>
-                  <Typography level="title-sm" sx={{mt:0.1,mb: 2}}>
-                   <Link href="" textColor={"neutral.700"} startDecorator={<CallIcon/>}>+998950010001</Link>
-                  </Typography>
-                  <CardOverflow
+                <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
+                  The Bruk Restaurants
+                </Typography>
+                <Typography level="title-sm" sx={{ mt: 0.1, mb: 2 }}>
+                  <Link
+                    href=""
+                    textColor={"neutral.700"}
+                    startDecorator={<LocationOnRoundedIcon />}
+                  >
+                    Turkey,Istanbul
+                  </Link>
+                </Typography>
+                <Typography level="title-sm" sx={{ mt: 0.1, mb: 2 }}>
+                  <Link
+                    href=""
+                    textColor={"neutral.700"}
+                    startDecorator={<CallIcon />}
+                  >
+                    +998950010001
+                  </Link>
+                </Typography>
+                <CardOverflow
                   sx={{
                     display: "flex",
                     gap: 1.5,
@@ -202,7 +229,7 @@ const BestRestaurants = () => {
                     >
                       100{" "}
                       <VisibilityIcon
-                        sx={{ fontSize: 20, marginLeft: "5px"}}
+                        sx={{ fontSize: 20, marginLeft: "5px" }}
                       />
                     </Typography>
                     <Box
@@ -223,9 +250,7 @@ const BestRestaurants = () => {
                       }}
                     >
                       <div>500</div>
-                      <Favorite
-                        sx={{ fontSize: 20, marginLeft: "5px" }}
-                      />
+                      <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
                     </Typography>
                   </Box>
                 </CardOverflow>
@@ -242,34 +267,46 @@ const BestRestaurants = () => {
                   <AspectRatio ratio={1}>
                     <img src="restaurant/girl.png" alt="" />
                     <IconButton
-                    aria-label="Like minimal photography"
-                    size="md"
-                    variant="solid"
-                    color="neutral"
-                    sx={{
-                      position: "absolute",
-                      zIndex: 2,
-                      borderRadius: "50%",
-                      right: "1rem",
-                      bottom: 0,
-                      transform: "translateY(50%)",
-                      color: "rgba(0,0,0,.4)",
-                    }}
-                  >
-                    <Favorite style={{ fill: "white" }} />
-                  </IconButton>
+                      aria-label="Like minimal photography"
+                      size="md"
+                      variant="solid"
+                      color="neutral"
+                      sx={{
+                        position: "absolute",
+                        zIndex: 2,
+                        borderRadius: "50%",
+                        right: "1rem",
+                        bottom: 0,
+                        transform: "translateY(50%)",
+                        color: "rgba(0,0,0,.4)",
+                      }}
+                    >
+                      <Favorite style={{ fill: "white" }} />
+                    </IconButton>
                   </AspectRatio>
                 </CardOverflow>
-                <Typography level="h2" sx={{fontSize:"md",mt:2}}>
-                   The Bruk Restaurants
-                  </Typography>
-                  <Typography level="title-sm" sx={{mt:0.1,mb: 2}}>
-                   <Link href="" textColor={"neutral.700"} startDecorator={<LocationOnRoundedIcon/>}>Turkey,Istanbul</Link>
-                  </Typography>
-                  <Typography level="title-sm" sx={{mt:0.1,mb: 2}}>
-                   <Link href="" textColor={"neutral.700"} startDecorator={<CallIcon/>}>+998950010001</Link>
-                  </Typography>
-                  <CardOverflow
+                <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
+                  The Bruk Restaurants
+                </Typography>
+                <Typography level="title-sm" sx={{ mt: 0.1, mb: 2 }}>
+                  <Link
+                    href=""
+                    textColor={"neutral.700"}
+                    startDecorator={<LocationOnRoundedIcon />}
+                  >
+                    Turkey,Istanbul
+                  </Link>
+                </Typography>
+                <Typography level="title-sm" sx={{ mt: 0.1, mb: 2 }}>
+                  <Link
+                    href=""
+                    textColor={"neutral.700"}
+                    startDecorator={<CallIcon />}
+                  >
+                    +998950010001
+                  </Link>
+                </Typography>
+                <CardOverflow
                   sx={{
                     display: "flex",
                     gap: 1.5,
@@ -306,7 +343,7 @@ const BestRestaurants = () => {
                     >
                       100{" "}
                       <VisibilityIcon
-                        sx={{ fontSize: 20, marginLeft: "5px"}}
+                        sx={{ fontSize: 20, marginLeft: "5px" }}
                       />
                     </Typography>
                     <Box
@@ -327,14 +364,135 @@ const BestRestaurants = () => {
                       }}
                     >
                       <div>500</div>
-                      <Favorite
+                      <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
+                    </Typography>
+                  </Box>
+                </CardOverflow>
+              </Card>
+              <Card
+                sx={{
+                  minHeight: 483,
+                  width: 320,
+                  mr: "35px",
+                  cursor: "pointer",
+                }}
+              >
+                <CardOverflow>
+                  <AspectRatio ratio={1}>
+                    <img src="restaurant/girl.png" alt="" />
+                    <IconButton
+                      aria-label="Like minimal photography"
+                      size="md"
+                      variant="solid"
+                      color="neutral"
+                      sx={{
+                        position: "absolute",
+                        zIndex: 2,
+                        borderRadius: "50%",
+                        right: "1rem",
+                        bottom: 0,
+                        transform: "translateY(50%)",
+                        color: "rgba(0,0,0,.4)",
+                      }}
+                    >
+                      <Favorite style={{ fill: "white" }} />
+                    </IconButton>
+                  </AspectRatio>
+                </CardOverflow>
+                <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
+                  The Bruk Restaurants
+                </Typography>
+                <Typography level="title-sm" sx={{ mt: 0.1, mb: 2 }}>
+                  <Link
+                    href=""
+                    textColor={"neutral.700"}
+                    startDecorator={<LocationOnRoundedIcon />}
+                  >
+                    Turkey,Istanbul
+                  </Link>
+                </Typography>
+                <Typography level="title-sm" sx={{ mt: 0.1, mb: 2 }}>
+                  <Link
+                    href=""
+                    textColor={"neutral.700"}
+                    startDecorator={<CallIcon />}
+                  >
+                    +998950010001
+                  </Link>
+                </Typography>
+                <CardOverflow
+                  sx={{
+                    display: "flex",
+                    gap: 1.5,
+                    py: 1.5,
+                    px: "var(--Card-padding)",
+                    borderTop: "1px solid",
+                  }}
+                >
+                  <IconButton
+                    aria-label="Like minimal photography"
+                    size="md"
+                    variant="solid"
+                    color="neutral"
+                    sx={{
+                      position: "absolute",
+                      zIndex: 2,
+                      borderRadius: "50%",
+                      right: "1rem",
+                      bottom: 45,
+                      transform: "translateY(50%)",
+                      color: "rgba(0,0,0,.4)",
+                    }}
+                  >
+                    <Favorite style={{ fill: "white" }} />
+                  </IconButton>
+                  <Box sx={{ display: "flex", flexDirection: "row" }}>
+                    <Typography
+                      level="title-md"
+                      sx={{
+                        fontWeight: "md",
+                        alignItems: "center",
+                        display: "flex",
+                      }}
+                    >
+                      100{" "}
+                      <VisibilityIcon
                         sx={{ fontSize: 20, marginLeft: "5px" }}
                       />
+                    </Typography>
+                    <Box
+                      sx={{
+                        width: 2,
+                        height: 20,
+                        bgcolor: "divider",
+                        margin: " 0 10px",
+                        color: "text.secondary",
+                      }}
+                    />
+                    <Typography
+                      sx={{
+                        fontWeight: "md",
+                        color: "text.secondary",
+                        alignItems: "center",
+                        display: "flex",
+                      }}
+                    >
+                      <div>500</div>
+                      <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
                     </Typography>
                   </Box>
                 </CardOverflow>
               </Card>
             </CssVarsProvider>
+          </Stack>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"flex-end"}
+            sx={{ width: "100%", mt: "16px" }}
+          >
+            <Button style={{ background: "#1976d2", color: "white" }}>
+              BARCHASINI KO’RISH
+            </Button>
           </Stack>
         </Stack>
       </Container>
