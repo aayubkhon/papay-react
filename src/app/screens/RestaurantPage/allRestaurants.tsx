e3c85ea0d5ec0b9c5398ff3e231b238a4189ac83
aayubxon701 <aayubxon701@gmail.com>
2023-11-19 23:35:28 +0900

    feat:restaurant page allrestaurant components page

--- src/app/screens/RestaurantPage/allRestaurants.tsx
+++ src/app/screens/RestaurantPage/allRestaurants.tsx
@@ -1,9 +1,198 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Button, Container, Stack } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { CssVarsProvider } from "@mui/joy/styles";
+import Card from "@mui/joy/Card";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+import IconButton from "@mui/joy/IconButton";
+import Favorite from "@mui/icons-material/Favorite";
+import Typography from "@mui/joy/Typography";
+import Link from "@mui/joy/Link";
+import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
+import CallIcon from "@mui/icons-material/Call";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+const order_list = Array.from(Array(8).keys());
 const AllRestaurants = () => {
   return (
-    <div>AllRestaurants</div>
-  )
-}
+    <div className="all_restaurant">
+      <Container>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Box className="fil_search_box">
+            <Box className="fil_box">
+              <a href="">Zo'r</a>
+              <a href="">Mashhur</a>
+              <a href="">Trenddagi</a>
+              <a href="">Yangi</a>
+            </Box>
+            <Box className="search_big_box">
+              <form action="" className="search_form">
+                <input
+                  type="search"
+                  className="searchInput"
+                  name="resSearch"
+                  placeholder="Qidiruv"
+                />
+                <Button
+                  className="button_search"
+                  variant="contained"
+                  endIcon={<SearchIcon />}
+                >
+                  Izlash
+                </Button>
+              </form>
+            </Box>
+          </Box>
+          <Stack className="all_res_box">
+            <CssVarsProvider>
+              {order_list.map((ele) => {
+                return (
+                  <Card
+                    variant="outlined"
+                    sx={{
+                      minHeight: 410,
+                      minWidth: 290,
+                      mx: "17px",
+                      my: "20px",
+                    }}
+                  >
+                    <CardOverflow>
+                      <AspectRatio ratio={1}>
+                        <img src="/restaurant/girl.png" alt="" />
+                      </AspectRatio>
+                      <IconButton
+                        area-label="Like minimal photography"
+                        size="md"
+                        variant="solid"
+                        color="neutral"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                        }}
+                        sx={{
+                          position: "absolute",
+                          zIndex: 2,
+                          borderRadius: "50%",
+                          right: "1rem",
+                          bottom: -16,
+                          transform: "traslate(50%)",
+                          color: "rgba(0, 0, 0, .4)",
+                        }}
+                      >
+                        <Favorite style={{ color: "#ffffff" }} />
+                      </IconButton>
+                    </CardOverflow>
+                    <Typography level="h2" sx={{ fontSize: "md", mt: 1 }}>
+                      Texas De Brazil restaurant
+                    </Typography>
+                    <Typography level="body-md" sx={{ mt: 0 }}>
+                      <Link
+                        href=""
+                        startDecorator={<LocationOnRoundedIcon />}
+                        textColor="neutral.700"
+                      >
+                        Tashkent Yunusabad 3-1
+                      </Link>
+                    </Typography>
+                    <Typography level="body-md" sx={{ mt: 0, mb: 1 }}>
+                      <Link
+                        href=""
+                        startDecorator={<CallIcon />}
+                        textColor="neutral.700"
+                      >
+                        99895001005001
+                      </Link>
+                    </Typography>
+                    <CardOverflow
+                      variant="soft"
+                      sx={{
+                        display: "flex",
+                        gap: 1.5,
+                        py: 1.5,
+                        px: "var(--Card-padding)",
+                        borderTop: "1px solid rgba(0,0,0,.1)",
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "row",
+                          gap: "10px",
+                        }}
+                      >
+                        <Typography
+                          level="body-sm"
+                          sx={{
+                            fontWeight: "md",
+                            color: "text.secondary",
+                            alignItems: "center",
+                            display: "flex",
+                          }}
+                        >
+                          1000{" "}
+                          <VisibilityIcon
+                            sx={{ fontSize: 20, marginLeft: "5px" }}
+                          />
+                        </Typography>
+                        <Box sx={{ width: 2, bgcolor: "divider" }} />
+                        <Typography
+                          level="body-sm"
+                          sx={{
+                            fontWeight: "md",
+                            color: "text.secondary",
+                            alignItems: "center",
+                            display: "flex",
+                          }}
+                        >
+                          <div>500</div>
+                          <FavoriteIcon
+                            sx={{ fontSize: 20, marginLeft: "5px" }}
+                          />
+                        </Typography>
+                      </Box>
+                    </CardOverflow>
+                  </Card>
+                );
+              })}
+            </CssVarsProvider>
+          </Stack>
 
-export default AllRestaurants
+          <Stack className="bottom_box">
+            <img
+            alt=""
+              className="design_img"
+              src="/icons/line.svg"
+              style={{ transform: "matrix(1, 0, 0, -1, 0, 0)" }}
+            />
+            <Pagination
+              count={3}
+              page={1}
+              renderItem={(item) => (
+                <PaginationItem
+                  components={{
+                    previous: ArrowBackIcon,
+                    next: ArrowForwardIcon,
+                  }}
+                  {...item}
+                  color="secondary"
+                />
+              )}
+            />
+            <img
+            alt=""
+              className="design_img"
+              src="/icons/line.svg"
+              style={{ transform: "rotate(180deg)" }}
+            />
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
+};
+
+export default AllRestaurants;
--- src/app/screens/RestaurantPage/index.tsx
+++ src/app/screens/RestaurantPage/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Container } from "@mui/material";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import ChoosenDish from "./choosenDish";
 import OneRestaurants from "./oneRestaurants";
 import AllRestaurants from "./allRestaurants";
+import "../../css/restaurant.css"
 
 export function RestaurantPage() {
   let restaurant = useRouteMatch();
